59ef8c773d60cb57b780eea6f32f2d8a03ad5e75
2laJ2 <tuulajakobsson@hotmail.com>
2022-03-21 16:58:32 +0200

    refactor tennis

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -2,29 +2,28 @@ class TennisGame:
     def __init__(self, player1_name, player2_name):
         self.player1_name = player1_name
         self.player2_name = player2_name
-        self.m_score1 = 0
-        self.m_score2 = 0
+        self.player1_score = 0
+        self.player2_score = 0
 
     def won_point(self, player_name):
         if player_name == "player1":
-            self.m_score1 += 1
+            self.player1_score += 1
         else:
-            self.m_score2 += 1
+            self.player2_score += 1
 
     def deuce_score(self):
-        if  self.m_score1 == 0:
+        if  self.player1_score == 0:
                 return "Love-All"
-        if  self.m_score1 == 1:
+        if  self.player1_score == 1:
                 return "Fifteen-All"
-        if  self.m_score1 == 2:
+        if  self.player1_score == 2:
                 return "Thirty-All"
-        if  self.m_score1 == 3:
+        if  self.player1_score == 3:
                 return "Forty-All"
         return "Deuce"
 
     def advantage_score(self):
-        difference = self.m_score1 - self. m_score2
-
+        difference = self.player1_score - self.player2_score
         if difference == 1:
             return "Advantage player1"
         if difference == -1:
@@ -33,28 +32,20 @@ class TennisGame:
             return "Win for player1"
         return "Win for player2"
 
-    def player_score(self, temp_score):
-        if temp_score == 0:
+    def player_score(self, score):
+        if score == 0:
             return "Love"
-        if temp_score == 1:
+        if score == 1:
             return "Fifteen"
-        if temp_score == 2:
+        if score == 2:
             return "Thirty"
-        if temp_score == 3:
+        if score == 3:
             return "Forty"
 
     def get_score(self):
-        if self.m_score1 == self.m_score2:
+        if self.player1_score == self.player2_score:
             return self.deuce_score()
-        if self.m_score1 >= 4 or self.m_score2 >= 4:
+        if self.player1_score >= 4 or self.player2_score >= 4:
             return self.advantage_score()
+        return f"{self.player_score(self.player1_score)}-{self.player_score(self.player2_score)}"
         
-        #score = ""
-        for i in range(1, 3):
-            return f"{self.player_score(self.m_score1)}-{self.player_score(self.m_score2)}"
-            #if i == 1:
-            #    score += self.player_score(self.m_score1)
-            #else:
-            #    score += "-"+self.player_score(self.m_score2)
-            
-        #return score
